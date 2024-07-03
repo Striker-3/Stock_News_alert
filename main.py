d76db58513da0c4340494305464dca3b6026cbdc
Nishant Wailkar <nishantw631@gmail.com>
2024-07-03 10:48:21 +0530

    added email sending functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import newsapi
 import requests
+import smtplib
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -11,79 +12,64 @@ api_key_alphavantage = "POFBA2EN11KWWTA7"
 api_key_news = "8f3ba393bf1e4bd3a9dca8cd66082fd4"
 
 parameters_alphavantage = {
-   "function":'TIME_SERIES_DAILY',
-    "symbol":STOCK_NAME,
-    "apikey":api_key_alphavantage,
+    "function": 'TIME_SERIES_DAILY',
+    "symbol": STOCK_NAME,
+    "apikey": api_key_alphavantage,
 }
-response = requests.get(url="https://www.alphavantage.co/query" , params=parameters_alphavantage)
+response = requests.get(url="https://www.alphavantage.co/query", params=parameters_alphavantage)
 
 response.raise_for_status()
-#
-# data = response.json()['Time Series (Daily)']
+
+data = response.json()['Time Series (Daily)']
 # print(response.json())
 
-# data_list = [value for (key , value) in data.items()]
+data_list = [value for (key, value) in data.items()]
 
-
-
-
-
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
-# yesterday_closing_price = data_list[0]['4. close']
+yesterday_closing_price = data_list[0]['4. close']
 # print(yesterday_closing_price)
 
-#TODO 2. - Get the day before yesterday's closing stock price
-# day_before_yesterday_closing_price = data_list[1]['4. close']
+day_before_yesterday_closing_price = data_list[1]['4. close']
 # print(day_before_yesterday_closing_price)
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-# difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 # print(difference)
 
+percent_diff = (difference / float(yesterday_closing_price)) * 100
+percent_diff_formatted = round(percent_diff, 2)
+# print(percent_diff_formatted)
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-# percent_diff = (difference / float(yesterday_closing_price)) * 100
-# print(percent_diff)
+if percent_diff > 5:
+    news_params = {
+        'q': "tesla",
+        'apiKey': api_key_news
+    }
 
-# #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-# if percent_diff > 5:
-news_params = {
-        'q':"tesla",
-        'apiKey':api_key_news
-}
+    response2 = requests.get(url="https://newsapi.org/v2/everything", params=news_params)
+    response2.raise_for_status()
+    articles = response2.json()['articles']
 
-response2 = requests.get(url="https://newsapi.org/v2/everything",params=news_params)
-response2.raise_for_status()
-articles = response2.json()['articles']
-# print(articles)
+    three_articles = articles[:3]
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    new_list_articles = [f"Headline: {article['title']}. \n Brief: {article['description']} " for article in
+                         three_articles]
+    # print(new_list_articles)
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-three_articles = articles[:3]
+    my_email = "www.nishantwailkar71@gmail.com"
+    password = "goub ejpi fbtf pryc"
+    if yesterday_closing_price > day_before_yesterday_closing_price:
+        arrow = "🔺"
+    else:
+        arrow = "🔻"
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number.
+    msg = f"Subject: TSLA {arrow}{percent_diff_formatted}\n\n"
+    for article in new_list_articles:
+        msg += f"{article}\n\n"
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-new_list_articles = [f"Headline: {article['title']}. \n Brief: {article['description']} " for article in three_articles]
-print(new_list_articles)
-#TODO 9. - Send each article as a separate message via Twilio.
-
-
-
-#Optional TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="nishantw631@gmail.com",
+            msg=msg.encode('utf-8')
+        )
